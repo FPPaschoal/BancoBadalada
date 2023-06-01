585c9f5b97f726cf814e63f2fa5d9bce93595f2b
Felipe Paschoal <112896945+FPPaschoal@users.noreply.github.com>
2023-06-01 17:59:26 -0300

    TerminarEmpregadoService

---
+++ BancoBadalada/BancoBadalada/Controllers/EmpregadoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BancoBadalada.Controllers
+{
+    public class EmpregadoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
