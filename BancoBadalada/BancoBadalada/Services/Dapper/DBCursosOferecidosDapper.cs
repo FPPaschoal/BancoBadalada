a4555063b7ef499712768362888b78cc210c01c6
ThalitaLobo <thalitalobopaz1@gmail.com>
2023-07-13 20:17:37 -0300

    update, find e findAll CO com dapper

--- BancoBadalada/BancoBadalada/Services/Dapper/DBCursosOferecidosDapper.cs
+++ BancoBadalada/BancoBadalada/Services/Dapper/DBCursosOferecidosDapper.cs
@@ -1,6 +1,7 @@
 ﻿using BancoBadalada.Models;
 using BancoBadalada.Services.Dapper.DATA;
 using BancoBadalada.Services.Interfaces;
+using Dapper;
 using System.Data;
 
 namespace BancoBadalada.Services.Dapper
@@ -23,14 +24,39 @@ namespace BancoBadalada.Services.Dapper
             throw new NotImplementedException();
         }
 
-        public TbCursosOferecidos Find(TbCursosOferecidos services)
+        public TbCursosOferecidos Find(TbCursosOferecidos cursoOferecido)
         {
-            throw new NotImplementedException();
+            var connection = _connection.CreateConnection();
+
+            string query = @"SELECT id_curso AS IdCurso,
+                                    dt_inicio AS DtInicio,
+                                    id_instrutor AS IdInstrutor,
+                                    localizacao AS Localizacao,
+                                    fg_ativo AS FgAtivo
+                             FROM tb_cursos_oferecidos
+                             WHERE id_curso = @IdCurso AND dt_inicio = @DtIncio";
+
+            var resultado = connection.Query<TbCursosOferecidos>(query, new {IdCurso = cursoOferecido.IdCurso, DtIncio = cursoOferecido.DtInicio}).FirstOrDefault();
+
+            var result = connection.QueryFirstOrDefault<TbCursosOferecidos>(query, new { IdCurso = cursoOferecido.IdCurso, DtIncio = cursoOferecido.DtInicio });
+
+            return result;
         }
 
         public ICollection<TbCursosOferecidos> FindAll()
         {
             var connection = _connection.CreateConnection();
+
+            string query = @"SELECT id_curso AS IdCurso,
+                                    dt_inicio AS DtInicio,
+                                    id_instrutor AS IdInstrutor,
+                                    localizacao AS Localizacao,
+                                    fg_ativo AS FgAtivo
+                             FROM tb_cursos_oferecidos";
+
+            var resultado = connection.Query<TbCursosOferecidos>(query).ToList();
+
+            return resultado;
         }
 
         public int GetNextId()
@@ -38,9 +64,17 @@ namespace BancoBadalada.Services.Dapper
             throw new NotImplementedException();
         }
 
-        public void Update(TbCursosOferecidos services)
+        public void Update(TbCursosOferecidos cursosOferecidos)
         {
-            throw new NotImplementedException();
+            var connection = _connection.CreateConnection();
+
+            string query = @"UPDATE tb_cursos_oferecidos
+                             SET fg_ativo = @FgAtivo,
+                                 id_instrutor = @IdInstrutor,
+                                 localizacao = @Localizacao
+                             WHERE id_curso = @IdCurso AND dt_inicio = @DtInicio";
+
+            connection.Execute(query, cursosOferecidos);
         }
     }
 }
